d51b67c1b1be283892594ee0f4823c8650ed898b
Brandon Ogle <brandon@standardcognition.com>
2023-10-11 17:26:08 -0700

    Mix training between single and duel games

--- spawn_games.sh
+++ spawn_games.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
 # Number of instances you want to run in parallel
-n=64
+n=16
+H=4
+W=4
 
 # Create a function to run battlesnake instances
+run_battlesnake_single() {
+    battlesnake play --height $H --width $W -n player1 -u http://localhost:3000
+}
+
+run_battlesnake_multi() {
+    battlesnake play --height $H --width $W -n player1 -u http://localhost:3000 -n player2 -u http://localhost:3000
+}
+
 run_battlesnake() {
-    battlesnake --verbose play -v -s -n player1 -u http://localhost:3000 -n player2 -u http://localhost:3000
+    rand=$(($RANDOM%1))
+    if [ $rand -eq 0 ]; then
+        run_battlesnake_multi
+    else
+        run_battlesnake_single
+    fi
 }
 
 # Start the initial set of processes
